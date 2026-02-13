5f8b6a0a64142b7ea5620d6c8f14e72173e52827
isam0322 <isam.t0322@gmail.com>
2026-02-13 20:01:08 +0900

    Add files via upload

---
+++ quotation-app/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'quotation-app-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './icon.svg'
+];
+
+// Install Event: Cache assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event: Serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
